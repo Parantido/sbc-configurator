703df73039ce3a7cef4e9833edd1aea0620c021c
Parantido De Rica <noreply>
2015-03-15 17:40:48 +0000

    Add component login

---
+++ ui/login.reel/login.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/login.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Login
+ * @extends Component
+ */
+exports.Login = Component.specialize(/** @lends Login# */ {
+    constructor: {
+        value: function Login() {
+            this.super();
+        }
+    }
+});
